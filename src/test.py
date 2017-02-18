562cb2b6dd8fe7791f2690c6bdbbd0ad56bc242e
fourohfour <404.fourohfour@gmail.com>
2017-02-18 00:26:58 +0000

    Deleting some test files that shouldn't have been pushed -> Delete id -> Delete test.py

--- src/test.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-response = requests.get("https://api.showmyhomework.co.uk/api/calendars?classGroup=7A%2FGg1",
-                                headers = {"Accept" : "application/smhw.v3+json"})
-
-print(response.text)
